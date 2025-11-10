1dec377965fb8bb52aa73e7477e932bd64583a3f
tliebold <tim.liebold@mail.de>
2025-11-10 15:19:46 +0100

    Sterne in Komponente ausgelagert

---
+++ database/migrations/2025_11_10_141610_add_user_to_movie_ratings.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('movie_ratings', function (Blueprint $table) {
+            $table->addColumn('string', 'user_id')->after('imdb_id');
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('rating', function (Blueprint $table) {
+            //
+        });
+    }
+};
